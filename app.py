51a0d3d7de43a6b7bf55c8e090b8a56fc2148a45
Raghav Sharma <raghaaav5@gmail.com>
2024-06-17 03:01:21 +0530

    Added Course Material, UI Improvements
    Added course material for Web dev, app dev and design
    
    Created new WIP pages for motion graphics and AI/ML
    
    Improved UI on home page

--- app.py
+++ app.py
@@ -9,11 +9,11 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("index.html", title="The Unofficial Resource Repo")
+    return render_template("index.html", title="The Unofficial ISTE Resource Repository")
 
 @app.route("/design")
 def design():
-    return render_template("display.html", title="Design & Motion Graphics", resources=design_res)
+    return render_template("display.html", title="Design", resources=design_res)
 
 @app.route("/web")
 def webdev():
@@ -22,6 +22,16 @@ def webdev():
 @app.route("/app")
 def appdev():
     return render_template("display.html", title="App Development", resources=app_res)
+
+
+@app.route("/aiml")
+def aiml():
+    return render_template("wip.html", title="AI/ML")
+
+@app.route("/motiongraphics")
+def motion():
+    return render_template("wip.html", title="Motion Graphics")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static.py
+++ static.py
@@ -5,16 +5,34 @@ design_res = [{"title": "Design Fundamentals", "type" : "video", "url": "https:/
               {"title": "Typography", "type" : "video", "url": "https://www.youtube.com/embed/sByzHoiYFX0?si=YRC0lqYnVW4k71cN"},
               {"title": "Branding & Identity", "type" : "video", "url": "https://www.youtube.com/embed/l-S2Y3SF3mM?si=ETfrs3BZh3x8KFPP"},
               {"title": "Images", "type" : "video", "url": "https://www.youtube.com/embed/MELKuexR3sQ?si=RW5bxjwYYIDeWfek"},
+              {"title": "Understanding Color for UI Design", "type" : "article", "url": "https://medium.com/rutgers-creative-x/understanding-color-for-ui-design-ec53719e880e"},              
               {"title": "DesignTropes Discord Server (Inspiration by Design Style)", "type" : "inspo", "url": "https://discord.gg/yMvkNVtt"},
               {"title": "Behance - All sorts of design Inspo", "type" : "inspo", "url": "https://www.behance.com/websites/"},
               {"title": "Awwwards - UI/UX Inspo", "type" : "inspo", "url": "https://www.awwwards.com/websites/"}
               ]
 
 
-web_res = [{"title": "Lorem Ipsum", "type" : "video", "url": "vnjdvjkdnbv"},{"title": "Lorem Ipsum", "type" : "article", "url": "vnjdvjkdnbv"}]
+web_res = [{"title": "CS50 Django", "type" : "video", "url": "https://www.youtube.com/embed/w8q0C-C1js4?si=Ruvl7HEEiqp2ntaa"},
+           {"title": "Sigma WebDev", "type" : "video", "url": "https://www.youtube.com/embed/videoseries?si=hY284F2O1mkN59pE&amp;list=PLu0W_9lII9agq5TrH9XLIKQvv0iaF2X3w"},
+           {"title": "CS50 Django", "type" : "video", "url": "https://www.youtube.com/embed/videoseries?si=liTAI2bWd5cHPR9o&amp;list=PLdHg5T0SNpN3EoN3PEyCmPR42Ok_44OFT"},
+           {"title": "CS50 Django", "type" : "video", "url": "https://www.youtube.com/embed/videoseries?si=j5UlQ2A0ngCrgSxe&amp;list=PLuHGmgpyHfRyL233CDEALV1BN6Xwqwb99"},
+           {"title": "CS50 Django", "type" : "video", "url": "https://www.youtube.com/embed/videoseries?si=h6AurzRhmNfJ_Oy_&amp;list=PLzjZaW71kMwScTRKzoasdyB1sX-a9EbFp"},
+           {"title": "NextJS", "type" : "video", "url": "https://www.youtube.com/embed/vCOSTG10Y4o?si=F_AzKq9-__D23KYf"},
+           {"title": "NextJS", "type" : "video", "url": "https://www.youtube.com/embed/erEgovG9WBs?si=D8zAvTEuitNpwbJF"},
+           {"title": "GeeksforGeeks Docs", "type" : "article", "url": "https://www.geeksforgeeks.org/"},
+           {"title": "MDN Docs", "type" : "article", "url": "https://developer.mozilla.org/en-US/"},
+           {"title": "TailwindCSS Docs", "type" : "article", "url": "https://tailwindcss.com/"},
+           {"title": "Awwwards- Web Design Inspo", "type" : "inspo", "url": "https://www.awwwards.com/"}]
 
 app_res = [{"title": "Kotlin Basics", "type" : "video", "url": "https://www.youtube.com/embed/AeC4G-H-MQA?si=KrdoJ66WBf9g3q1M"},
-           {"title": "Kotlin Basics", "type" : "video", "url": "https://www.youtube.com/embed/AeC4G-H-MQA?si=KrdoJ66WBf9g3q1M"},
+           {"title": "Kotlin Basics", "type" : "video", "url": "https://www.youtube.com/embed/qvDo0SKR8-k?si=MBOXl2W4x4U0jy_b"},
+           {"title": "Phillip Lackner", "type" : "video", "url": "https://www.youtube.com/embed/videoseries?si=i_wiGDbP7pxD7idy&amp;list=PLQkwcJG4YTCSVDhww92llY3CAnc_vUhsm"},
+           {"title": "Phillip Lackner", "type" : "video", "url": "https://www.youtube.com/embed/VPvVD8t02U8?si=UxL0heG6hkjOkM7E"},
            {"title": "Android Developers Official", "type" : "article", "url": "https://developer.android.com/get-started/overview"},
+           {"title": "Flutter Docs", "type" : "article", "url": "https://docs.flutter.dev/"},
            {"title": "Ashwani's Special Notes", "type" : "article", "url": "https://immediate-amusement-e5a.notion.site/Android-App-Development-ecc44de08bf845368e1cef6cd22456f5"},
+           {"title": "Google's Material Design", "type" : "inspo", "url": "https://m3.material.io/"},
            {"title": "Beautiful Repository Consisting of Jetpack Compose Examples", "type" : "inspo", "url": "https://github.com/android/compose-samples/tree/main"}]
+
+
+
